1400b212576d91e4b765786a39a132895f4f1a6d
Mathias <mathias.richard.schneider@gmail.com>
2021-06-28 14:33:29 -0500

    updates dummy data to match backend schema

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,7 +2,7 @@
   <div class="index">
     <ul>
       <li v-for="student in students" v-bind:key="student.id" class="student-index">
-        <p>{{ student.name }}</p>
+        <p>{{ student.first_name }} {{ student.last_name }}</p>
       </li>
     </ul>
   </div>
@@ -14,29 +14,29 @@ export default {
   data: function () {
     return {
       students: [
-        { id: 1, name: "Sophie Fishman" },
-        { id: 2, name: "Ian Belfatto" },
-        { id: 3, name: "Mathias Schneider" },
+        { id: 1, first_name: "Sophie", last_name: "Fishman" },
+        { id: 2, first_name: "Ian", last_name: "Belfatto" },
+        { id: 3, first_name: "Mathias", last_name: "Schneider" },
       ],
       capstones: [
         {
           id: 1,
           student_id: 1,
-          title: "Premonition",
-          image_url:
+          name: "Premonition",
+          screenshot:
             "https://cdn.shopify.com/s/files/1/0257/6105/4792/products/Premonition-EtahLove-1_250x.jpg?v=1595602901",
         },
         {
           id: 2,
           student_id: 2,
-          title: "Travel Planner",
-          image_url: "https://chrisguillebeau.com/files/2016/10/landscape-aircraft-clouds-storm-38574-250x250.jpeg",
+          name: "Travel Planner",
+          screenshot: "https://chrisguillebeau.com/files/2016/10/landscape-aircraft-clouds-storm-38574-250x250.jpeg",
         },
         {
           id: 3,
           student_id: 3,
-          title: "Video Game Vault",
-          image_url: "https://s3-media0.fl.yelpcdn.com/bphoto/uwhGyQZNqa-1--wveLAc-g/ls.jpg",
+          name: "Video Game Vault",
+          screenshot: "https://s3-media0.fl.yelpcdn.com/bphoto/uwhGyQZNqa-1--wveLAc-g/ls.jpg",
         },
       ],
     };
