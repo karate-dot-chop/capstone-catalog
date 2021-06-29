2a0a2fcb270d6146d4cca496af15a8240922641d
ianbelfatto <ibelfatto@gmail.com>
2021-06-29 12:36:58 -0400

    adds show view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Index from "../views/Index.vue";
+import Show from "../views/Show.vue";
 
 Vue.use(VueRouter);
 
@@ -14,12 +15,10 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   { path: "/index", name: "Index", component: Index },
+  { path: "/show/:id", name: "Show", component: Show },
 ];
 
 const router = new VueRouter({
