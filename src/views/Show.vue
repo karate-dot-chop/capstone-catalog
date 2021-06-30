ba327cb7c2e069d84ea221f889037092838c219c
sophie <sophiefishman@gmail.com>
2021-06-30 11:40:25 -0500

    edits image size on show page

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -31,7 +31,8 @@ export default {
         name: "Premonition",
         screenshot:
           "https://cdn.shopify.com/s/files/1/0257/6105/4792/products/Premonition-EtahLove-1_250x.jpg?v=1595602901",
-        description: "Sci-Fi movie app.",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis iaculis porta. Proin tortor leo, pellentesque vel laoreet ac, egestas non ex. Ut urna lectus, suscipit ac ante euismod, rhoncus lacinia tellus. Proin pellentesque eget quam id suscipit. Proin enim odio, bibendum non iaculis nec, mattis vel risus. Praesent faucibus sit amet est eu scelerisque. Quisque lacinia nulla id velit finibus, in maximus velit cursus. Aenean diam mauris, euismod sed pharetra id, pellentesque a nibh. Nam posuere lectus vitae purus maximus rhoncus. Fusce tristique mauris eu placerat sollicitudin.",
       },
     };
   },
