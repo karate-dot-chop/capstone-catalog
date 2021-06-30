c94aebedde0d524bc0ca846d5ebf7fe04f64d041
ianbelfatto <ibelfatto@gmail.com>
2021-06-30 12:53:29 -0400

    adds footer and removes footer data for aesthetic purposes

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,9 @@
     <router-view />
 
     <!-- FOOTER -->
-    <!-- <footer id="footer">
+    <footer id="footer">
       <div class="inner">
-        <section>
+        <!-- <section>
           <h2>Get in touch</h2>
           <form method="post" action="#">
             <div class="fields">
@@ -35,44 +35,15 @@
               <li><input type="submit" value="Send" class="primary" /></li>
             </ul>
           </form>
-        </section>
-        <section>
-          <h2>Follow</h2>
-          <ul class="icons">
-            <li>
-              <a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon brands style2 fa-dribbble"><span class="label">Dribbble</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon brands style2 fa-500px"><span class="label">500px</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a>
-            </li>
-            <li>
-              <a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a>
-            </li>
-          </ul>
-        </section>
-        <ul class="copyright">
+        </section> -->
+        <!-- <ul class="copyright">
           <li>&copy; Untitled. All rights reserved</li>
-          <li>
-            Design:
+          <li> -->
+        <!-- Design:
             <a href="http://html5up.net">HTML5 UP</a>
           </li>
-        </ul>
+        </ul> -->
       </div>
-    </footer> -->
+    </footer>
   </div>
 </template>
